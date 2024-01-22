44e4a115e427133c7609673a037a3157deaaf872
Lada <SLada@ocvts.org>
2024-01-22 13:36:26 -0500

    d

--- graphs.R
+++ graphs.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+<<<<<<< HEAD
 library(janitor)
 library(ggthemes)
 library(ggridges)
@@ -23,3 +24,57 @@ ggplot(company_plants, aes(x = `capacity_latest`, y = `county`, fill = ..x..)) +
     panel.spacing = unit(0.1, "lines"),
     strip.text.x = element_text(size = 8)
   )
+=======
+library(ggthemes)
+library(janitor)
+library(hrbrthemes)
+library(dplyr)
+library(tidyr)
+library(viridis)
+
+#read in and pre-process file
+power_plant = read_csv("California_Power_Plants.csv")
+power_plant <- power_plant |> 
+  janitor::clean_names()
+power_plant <- power_plant |>
+  mutate(year = year(start_date)) |> 
+  arrange(year)
+
+#animated bar plot
+p <- ggplot(power_plant, aes(x = pri_energy_source, y = capacity_latest)) + 
+  geom_bar(stat = "identity")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+>>>>>>> a4a62bdf183fc17d6ec749c25144874b86bad894
