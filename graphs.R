80cdcf7ab89d604e78d3cad03ae18d15f2bb4ceb
Ava M <avagmcnabb@gmail.com>
2024-01-22 07:41:26 -0500

    graphs

---
+++ graphs.R
@@ -0,0 +1,53 @@
+library(tidyverse)
+library(ggthemes)
+library(janitor)
+library(hrbrthemes)
+library(dplyr)
+library(tidyr)
+library(viridis)
+
+#read in file
+power_plant = read_csv("California_Power_Plants.csv")
+power_plant <- power_plant |> 
+  janitor::clean_names()
+power_plant <- power_plant |>
+  mutate(year = year(start_date)) |> 
+  arrange(year)
+
+#animated bar plot
+p <- ggplot(power_plant, aes(x = pri_energy_source, y = capacity_latest)) + 
+  geom_bar(stat = "identity")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
